cae1b3d3c25a974132cdd3ca74efb8faf1598b88
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-06-25 19:51:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,103 +1,164 @@
-let totalAmount = document.getElementById("total-amount");
-let userAmount = document.getElementById("user-amount");
-const checkAmountButton = document.getElementById("check-amount");
-const totalAmountButton = document.getElementById("total-amount-button");
-const productTitle = document.getElementById("product-title");
-const errorMessage = document.getElementById("budget-error");
-const productTitleError = document.getElementById("product-title-error");
-const productCostError = document.getElementById("product-cost-error");
-const amount = document.getElementById("amount");
-const expenditureValue = document.getElementById("expenditure-value");
-const balanceValue = document.getElementById("balance-amount");
-const list = document.getElementById("list");
-let tempAmount = 0;
+// Sample initial transaction data
+let transactions = [];
+let currentUser = null;
 
-//Set Budget Part
-totalAmountButton.addEventListener("click", () => {
-  tempAmount = totalAmount.value;
-  //empty or negative input
-  if (tempAmount === "" || tempAmount < 0) {
-    errorMessage.classList.remove("hide");
-  } else {
-    errorMessage.classList.add("hide");
-    //Set Budget
-    amount.innerHTML = tempAmount;
-    //Set Balance
-    balanceValue.innerText = tempAmount - expenditureValue.innerText;
-    //Clear Input Box
-    totalAmount.value = "";
-  }
-});
+// Function to update the transaction table
+function updateTransactionTable() {
+  const table = document.getElementById("transactionTable");
+  table.innerHTML = `
+    <tr>
+      <th>Type</th>
+      <th>Description</th>
+      <th>Amount</th>
+      <th>Actions</th>
+    </tr>
+  `;
 
-//Function To Disable Edit and Delete Button
-const disableButtons = (bool) => {
-  let editButtons = document.getElementsByClassName("edit");
-  Array.from(editButtons).forEach((element) => {
-    element.disabled = bool;
+  transactions.forEach((transaction, index) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${transaction.type}</td>
+      <td>${transaction.description}</td>
+      <td>₹${transaction.amount.toFixed(2)}</td>
+      <td>
+        <button onclick="editTransaction(${index})">Edit</button>
+        <button onclick="deleteTransaction(${index})">Delete</button>
+      </td>
+    `;
+
+    table.appendChild(row);
   });
-};
+}
 
-//Function To Modify List Elements
-const modifyElement = (element, edit = false) => {
-  let parentDiv = element.parentElement;
-  let currentBalance = balanceValue.innerText;
-  let currentExpense = expenditureValue.innerText;
-  let parentAmount = parentDiv.querySelector(".amount").innerText;
-  if (edit) {
-    let parentText = parentDiv.querySelector(".product").innerText;
-    productTitle.value = parentText;
-    userAmount.value = parentAmount;
-    disableButtons(true);
-  }
-  balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
-  expenditureValue.innerText =
-    parseInt(currentExpense) - parseInt(parentAmount);
-  parentDiv.remove();
-};
+// Function to update the current balance
+function updateBalance() {
+  const balance = document.getElementById("balance");
+  const total = transactions.reduce((acc, transaction) => {
+    if (transaction.type === "Income") {
+      return acc + transaction.amount;
+    } else {
+      return acc - transaction.amount;
+    }
+  }, 0);
 
-//Function To Create List
-const listCreator = (expenseName, expenseValue) => {
-  let sublistContent = document.createElement("div");
-  sublistContent.classList.add("sublist-content", "flex-space");
-  list.appendChild(sublistContent);
-  sublistContent.innerHTML = `<p class="product">${expenseName}</p><p class="amount">${expenseValue}</p>`;
-  let editButton = document.createElement("button");
-  editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
-  editButton.style.fontSize = "1.2em";
-  editButton.addEventListener("click", () => {
-    modifyElement(editButton, true);
-  });
-  let deleteButton = document.createElement("button");
-  deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
-  deleteButton.style.fontSize = "1.2em";
-  deleteButton.addEventListener("click", () => {
-    modifyElement(deleteButton);
-  });
-  sublistContent.appendChild(editButton);
-  sublistContent.appendChild(deleteButton);
-  document.getElementById("list").appendChild(sublistContent);
-};
+  balance.textContent = `Current Balance: ₹${total.toFixed(2)}`;
+}
 
-//Function To Add Expenses
-checkAmountButton.addEventListener("click", () => {
-  //empty checks
-  if (!userAmount.value || !productTitle.value) {
-    productTitleError.classList.remove("hide");
-    return false;
-  }
-  //Enable buttons
-  disableButtons(false);
-  //Expense
-  let expenditure = parseInt(userAmount.value);
-  //Total expense (existing + new)
-  let sum = parseInt(expenditureValue.innerText) + expenditure;
-  expenditureValue.innerText = sum;
-  //Total balance(budget - total expense)
-  const totalBalance = tempAmount - sum;
-  balanceValue.innerText = totalBalance;
-  //Create list
-  listCreator(productTitle.value, userAmount.value);
-  //Empty inputs
-  productTitle.value = "";
-  userAmount.value = "";
-});
+// Function to add a new transaction
+function addTransaction(event) {
+  event.preventDefault();
+
+  const type = document.getElementById("type").value;
+  const description = document.getElementById("description").value;
+  const amount = parseFloat(document.getElementById("amount").value);
+
+  const transaction = {
+    type,
+    description,
+    amount
+  };
+
+  transactions.push(transaction);
+  updateTransactionTable();
+  updateBalance();
+
+  // Save transaction data to local storage
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+  document.getElementById("transactionForm").reset();
+}
+
+// Function to edit a transaction
+function editTransaction(index) {
+  const transaction = transactions[index];
+  const { type, description, amount } = transaction;
+
+  document.getElementById("type").value = type;
+  document.getElementById("description").value = description;
+  document.getElementById("amount").value = amount;
+
+  transactions.splice(index, 1);
+  updateTransactionTable();
+  updateBalance();
+
+  // Save transaction data to local storage
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+// Function to delete a transaction
+function deleteTransaction(index) {
+  transactions.splice(index, 1);
+  updateTransactionTable();
+  updateBalance();
+
+  // Save transaction data to local storage
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+// Function to create a user account
+function createAccount(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  currentUser = { name };
+
+  // Save user data to local storage
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+  document.getElementById("accountInfo").style.display = "none";
+  document.getElementById("transactionDetails").style.display = "block";
+  document.getElementById("welcomeMessage").textContent = `Welcome 👋🏻, ${name}!`;
+  const title = document.querySelector(".title");
+  title.remove();
+}
+
+// Function to delete user account
+function deleteAccount() {
+  currentUser = null;
+  transactions = [];
+
+  // Remove user and transaction data from local storage
+  localStorage.removeItem("currentUser");
+  localStorage.removeItem("transactions");
+
+  document.getElementById("accountInfo").style.display = "block";
+  document.getElementById("transactionDetails").style.display = "none";
+  document.getElementById("transactionTable").innerHTML = `
+    <tr>
+      <th>Type</th>
+      <th>Description</th>
+      <th>Amount</th>
+      <th>Actions</th>
+    </tr>
+  `;
+  document.getElementById("balance").textContent = "Current Balance: ₹0.00";
+  document.getElementById("transactionForm").reset();
+}
+
+// Attach event listeners
+document.getElementById("accountForm").addEventListener("submit", createAccount);
+document.getElementById("transactionForm").addEventListener("submit", addTransaction);
+document.getElementById("deleteAccountBtn").addEventListener("click", deleteAccount);
+
+// Check if user data exists in local storage
+const storedUserData = localStorage.getItem("currentUser");
+if (storedUserData) {
+  currentUser = JSON.parse(storedUserData);
+
+  document.getElementById("accountInfo").style.display = "none";
+  document.getElementById("transactionDetails").style.display = "block";
+  document.getElementById("welcomeMessage").textContent = `Welcome Back ❤️, ${currentUser.name}!`;
+  const title = document.querySelector(".title");
+  title.remove();
+}
+
+// Check if transaction data exists in local storage
+const storedTransactionData = localStorage.getItem("transactions");
+if (storedTransactionData) {
+  transactions = JSON.parse(storedTransactionData);
+  updateTransactionTable();
+  updateBalance();
+}
+
+
